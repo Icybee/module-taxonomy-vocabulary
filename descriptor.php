be051b93f054eaee9b967e5e7c0f4941de2729a4
Olivier Laviale <olivier.laviale@gmail.com>
2015-07-26 03:34:35 +0200

    Updated reauirements

--- descriptor.php
+++ descriptor.php
@@ -5,57 +5,48 @@ namespace Icybee\Modules\Taxonomy\Vocabulary;
 use ICanBoogie\ActiveRecord\Model;
 use ICanBoogie\Module\Descriptor;
 
-return array
-(
+return [
+
 	Descriptor::CATEGORY => 'organize',
 	Descriptor::DESCRIPTION => 'Manage vocabulary',
-	Descriptor::MODELS => array
-	(
-		'primary' => array
-		(
-			Model::SCHEMA => array
-			(
-				'fields' => array
-				(
-					'vid' => 'serial',
-					'siteid' => 'foreign',
-					'vocabulary' => 'varchar',
-					'vocabularyslug' => array('varchar', 80, 'indexed' => true),
-					'is_tags' => 'boolean',
-					'is_multiple' => 'boolean',
-					'is_required' => 'boolean',
+	Descriptor::MODELS => [
 
-					/**
-					 * Specify the weight of the element used to edit this vosabulary
-					 * in the altered edit block of the constructor.
-					 */
+		'primary' => [
 
-					'weight' => array('integer', 'unsigned' => true)
-				)
-			)
-		),
+			Model::SCHEMA => [
 
-		'scopes' => array
-		(
+				'vid' => 'serial',
+				'siteid' => 'foreign',
+				'vocabulary' => 'varchar',
+				'vocabularyslug' => [ 'varchar', 80, 'indexed' => true ],
+				'is_tags' => 'boolean',
+				'is_multiple' => 'boolean',
+				'is_required' => 'boolean',
+
+				/**
+				 * Specify the weight of the element used to edit this vosabulary
+				 * in the altered edit block of the constructor.
+				 */
+
+				'weight' => [ 'integer', 'unsigned' => true ]
+
+			]
+		],
+
+		'scopes' => [
+
 			Model::ACTIVERECORD_CLASS => 'ICanBoogie\ActiveRecord',
 			Model::CLASSNAME => 'ICanBoogie\ActiveRecord\Model',
-			Model::SCHEMA => array
-			(
-				'fields' => array
-				(
-					'vid' => array('foreign', 'primary' => true),
-					'constructor' => array('varchar', 64, 'primary' => true)
-				)
-			)
-		)
-	),
+			Model::SCHEMA => [
+
+				'vid' => [ 'foreign', 'primary' => true ],
+				'constructor' => [ 'varchar', 64, 'primary' => true ]
+
+			]
+		]
+	],
 
 	Descriptor::NS => __NAMESPACE__,
-	Descriptor::REQUIRES => array
-	(
-// 		'taxonomy.terms' => '1.x'
-	),
+	Descriptor::TITLE => "Vocabulary"
 
-	Descriptor::TITLE => 'Vocabulary',
-	Descriptor::VERSION => '1.0'
-);
+];
