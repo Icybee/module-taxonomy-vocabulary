65f9dda86e9e17277a9a5a3c8ae122617fb0ef82
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-23 02:29:55 +0200

    Event config fragments are now their own file

---
+++ config/event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Icybee\Modules\Taxonomy\Vocabulary;
+
+use Icybee;
+
+$hooks = Hooks::class . '::';
+
+return [
+
+	Icybee\Modules\Nodes\SaveOperation::class . '::process' => $hooks . 'on_node_save',
+	Icybee\Modules\Nodes\EditBlock::class . '::alter_children' => $hooks . 'on_nodes_editblock_alter_children',
+	Icybee\Modules\Nodes\Node::class . '::property' => $hooks . 'get_term',
+	Icybee\Modules\Pages\BreadcrumbElement::class . '::render_inner_html:before' => $hooks . 'before_breadcrumb_render_inner_html',
+//    Icybee\Modules\Views\Collection::class . '::collect' => $hooks . 'on_collect_views',
+//    Icybee\Modules\Views\Provider::class . '::alter_query' => $hooks . 'on_alter_provider_query',
+
+];
